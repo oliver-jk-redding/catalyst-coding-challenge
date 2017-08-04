61f9821b30c279a433d2236e00c5c9ef323f9af1
oliver-jk-redding <oliver.jk.redding@gmail.com>
2017-08-04 08:44:03 +0100

    add .gitignore and remove vagrant files from git because theyre only needed for development and shouldnt be in the remote

--- vagrant-provision-scripts/general.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get update
-sudo apt-get install -y language-pack-en
-sudo apt-get install -y vim curl python-software-properties
--- vagrant-provision-scripts/mysql.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-sudo apt-get -y install mysql-server
-sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
-mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES; CREATE DATABASE IF NOT EXISTS db;"
-sudo /etc/init.d/mysql restart
