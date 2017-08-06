89eaef1cef087ec0e1498b6ee5220020275b254f
oliver-jk-redding <oliver.jk.redding@gmail.com>
2017-08-06 07:37:27 +0100

    refactor code using classes

--- user_upload.php
+++ user_upload.php
@@ -28,79 +28,99 @@ require __DIR__ . '/vendor/autoload.php';
 use Aura\Cli\CliFactory;
 use Aura\Cli\Status;
 
-$cli_factory = new CliFactory;
-$context = $cli_factory->newContext($GLOBALS);
-$stdio = $cli_factory->newStdio();
+class CSV_To_Database_Reader {
 
-// Set the CLI options
-$options = array(
-    'file:',
-    'create_table',
-    'dry_run',
-    'u:',
-    'p:',
-    'h:',
-    'help'
-);
-$getopt = $context->getopt($options);
+	protected $stdio;   // Object to handle IO
+	protected $context; // Context for the CLI application with arguments
+	protected $cli_options = array(
+		'file:',
+		'create_table',
+		'dry_run',
+		'u:',
+		'p:',
+		'h:',
+		'help'
+	);
+	protected $getopt;	// Object that retrieves options passed as arguments in the Command Line
 
-// Get the CLI options used in the command line
-$file   		= $getopt->get(	'--file', 			false);
-$create_table   = $getopt->get(	'--create_table', 	false);
-$dry_run   		= $getopt->get(	'--dry_run', 		false);
-$user   		= $getopt->get(  '-u', 				false);
-$pass  			= $getopt->get(  '-p', 				false);
-$host   		= $getopt->get(  '-h', 				false);
-$help   		= $getopt->get(	'--help', 			false);
-// This variable will be filled if there are any illegal options used
-$illegal_arg 	= $getopt->get(	   1,				null);
+	function __construct() {
+		$cli_factory 	= new CliFactory;
+		$this->context 	= $cli_factory->newContext($GLOBALS);
+		$this->stdio 	= $cli_factory->newStdio();
+		$this->getopt 	= $this->context->getopt($this->cli_options);
 
-
-// Handle any CLI errors
-$table_exists = false;
-
-function _exit($status) {
-	exit('Exited with code '.$status.'.'.PHP_EOL);
-}
-
-if ($getopt->hasErrors()) {
-    $errors = $getopt->getErrors();
-    foreach ($errors as $error) {
-    	$stdio->errln('<<red>>Error: '.$error->getMessage().'<<reset>>');
-    }
-    _exit(Status::USAGE);
-};
-if($illegal_arg) {
-	$stdio->errln('<<red>>Error: The option \''.$illegal_arg.'\' is not defined.<<reset>>');
-	_exit(Status::USAGE);
-}
-if($help) {
-	if($argc > 2) {
-		$stdio->errln('<<red>>Error: The \'--help\' flag should not include any other arguments.<<reset>>');
-		_exit(Status::USAGE);
+		$this->init();
 	}
-	// display_help();
-}
-if($create_table) {
-	// create_table();
-	$table_exists = true;
-	$stdio->outln('<<green>>Users table added to DB.<<reset>>');
-	if(!$file) _exit(Status::SUCCESS);
-}
-if($file) {
-	if(!$table_exists && !$dry_run) {
-		$stdio->errln('<<red>>Error: Users table does not exist. Run \'php user_upload.php --create_table\', then run this command again.<<reset>>');
-		_exit(Status::UNAVAILABLE);
+
+	function init() {
+		$options = $this->get_options_from_command_line();
+		$this->handle_any_CLI_errors($options);
 	}
-	// $data = read_file($file);
-	if(!$dry_run) {
-		// save_data_to_DB($data);
-		$stdio->outln('<<green>>File successfully read into DB.users.<<reset>>');
-		_exit(Status::SUCCESS);
+
+	function get_options_from_command_line() {
+		// Get the CLI options used in the command line
+		return array(
+			'file'   		=> $this->getopt->get( '--file', 			false),
+			'create_table'  => $this->getopt->get( '--create_table', 	false),
+			'dry_run'   	=> $this->getopt->get( '--dry_run', 		false),
+			'user'   		=> $this->getopt->get(  '-u', 				false),
+			'pass'  		=> $this->getopt->get(  '-p', 				false),
+			'host'   		=> $this->getopt->get(  '-h', 				false),
+			'help'   		=> $this->getopt->get( '--help', 			false),
+			// This variable will be filled if there are any illegal options used
+			'illegal_arg' 	=> $this->getopt->get(	  1,				null),
+		);
 	}
-	$stdio->outln('<<green>>Successful dry run completed. No data added to DB.users.<<reset>>');
-	_exit(Status::SUCCESS);
+
+	function handle_any_CLI_errors($options) {
+		$table_exists = false;
+
+		if ($this->getopt->hasErrors()) {
+		    $errors = $this->getopt->getErrors();
+		    foreach ($errors as $error) {
+		    	$this->stdio->errln('<<red>>Error: '.$error->getMessage().'<<reset>>');
+		    }
+		    $this->exit(Status::USAGE);
+		};
+		if($options['illegal_arg']) {
+			$this->stdio->errln('<<red>>Error: The option \''.$options['illegal_arg'].'\' is not defined.<<reset>>');
+			$this->exit(Status::USAGE);
+		}
+		if($options['help']) {
+			if($argc > 2) {
+				$this->stdio->errln('<<red>>Error: The \'--help\' flag should not include any other arguments.<<reset>>');
+				$this->exit(Status::USAGE);
+			}
+			// $this->display_help();
+		}
+		if($options['create_table']) {
+			// $this->create_table();
+			$table_exists = true;
+			$this->stdio->outln('<<green>>Users table added to DB.<<reset>>');
+			if(!$options['file']) $this->exit(Status::SUCCESS);
+		}
+		if($options['file']) {
+			if(!$table_exists && !$options['dry_run']) {
+				$this->stdio->errln('<<red>>Error: Users table does not exist. Run \'php user_upload.php --create_table\', then run this command again.<<reset>>');
+				$this->exit(Status::UNAVAILABLE);
+			}
+			// $data = $this->read_file($options['file']);
+			if(!$options['dry_run']) {
+				// $this->save_data_to_DB($data);
+				$this->stdio->outln('<<green>>File successfully read into DB.users.<<reset>>');
+				$this->exit(Status::SUCCESS);
+			}
+			$this->stdio->outln('<<green>>Successful dry run completed. No data added to DB.users.<<reset>>');
+			$this->exit(Status::SUCCESS);
+		}
+		$this->stdio->errln('<<red>>Error: No file specified.<<reset>>');
+		$this->exit(Status::USAGE);
+	}
+
+	function exit($status) {
+		exit('Exited with code '.$status.'.'.PHP_EOL);
+	}
 }
-$stdio->errln('<<red>>Error: No file specified.<<reset>>');
-_exit(Status::USAGE);
+
+new CSV_To_Database_Reader();
 
